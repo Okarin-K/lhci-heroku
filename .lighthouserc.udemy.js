ec1ed9bf51a913aa40537c8b2c3a93bea8bd3086
Okarin-K <rachel9029@icloud.com>
2022-10-28 01:26:59 +0900

    update: 収集回数を2回に変更

--- .lighthouserc.udemy.js
+++ .lighthouserc.udemy.js
@@ -25,7 +25,7 @@ module.exports = {
             output: 'json',
             onlyCategories: ['performance'],
           },
-          numberOfRuns: 1
+          numberOfRuns: 2
       },
       upload: {
         target: "lhci",
